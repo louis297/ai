f8e3d0111708596c84bee3e6f13b7bc05abe9a1f
Xu Liu <louis@Xus-MacBook-Pro.local>
2017-04-07 19:36:13 +0800

    fix name typo

---
+++ l2c/l2c/algorithms/utilities.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# core algorithms interface
+# provide interface between main controller and algorithms part
+# Louis
+# 04/07/2017
+
+def help():
+    ret = '''.分科   查询分科信息
+帮助   查看帮助信息
+其他   AI闲聊模式
+          '''
+    return ret
